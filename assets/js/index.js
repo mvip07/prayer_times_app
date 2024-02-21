d0842af6f16318bce3dcfae6847d4225c558c864
Ozodov Mirafzal <90509996+mvip07@users.noreply.github.com>
2024-02-21 16:12:32 +0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,172 @@
+const APIBASE = "http://api.aladhan.com/v1/calendar/";
+
+const GEO = navigator.geolocation;
+let today = new Date();
+let month = `${today.getMonth() + 1 < 10 ? "0" : " "}${today.getMonth() + 1}`;
+let year = `${today.getFullYear()}`;
+
+const prayerContainer = document.querySelector(".prayer-default-slider");
+const select = document.querySelector(".select");
+
+GEO.getCurrentPosition(function (position) {
+  const { longitude, latitude } = position.coords;
+
+  if (longitude && latitude) {
+    fetch(
+      `${APIBASE}${year}/${month}?latitude=${latitude}&longitude=${longitude}&method=2`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        chooseButton(res.data);
+        add_prayer_time(res.data);
+        add_time_on_select(res.data);
+
+        get_loader();
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+function add_prayer_time(data) {
+  prayerContainer.innerHTML = "";
+  for (let i of data) {
+    prayerContainer.innerHTML += `
+        <div class="prayer__default--single">
+			<div class="prayer__date">
+				<p class="prayer__link">${i?.date.hijri.date}</p>
+				<span class="prayer__price">${i?.date.readable}</span>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Fajr:</p>
+				<p class="prayer__link">${i?.timings.Fajr}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Sunrise: </p>
+				<p class="prayer__link">${i?.timings.Sunrise}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Dhuhr:</p>
+				<p class="prayer__link">${i?.timings.Dhuhr}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Asr:</p>
+				<p class="prayer__link">${i?.timings.Asr}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Maghrib</p>
+				<p class="prayer__link">${i?.timings.Maghrib}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Sunset:</p>
+				<p class="prayer__link">${i?.timings.Sunset}</p>
+			</div>
+
+			<div class="prayer__times">
+				<p class="prayer__link">Isha:</p>
+				<p class="prayer__link">${i?.timings.Isha}</p>
+			</div>
+        </div>
+	`;
+  }
+}
+
+function chooseButton(prayerDate) {
+  const todayBtn = document.querySelector(".today");
+  const month = document.querySelector(".month");
+
+  todayBtn.addEventListener("click", () => {
+    const day = new Date().getUTCDate();
+    prayerDate.filter((i) =>
+      i.date.readable.substr(0, 2) == day ? add_prayer_time([i]) : ""
+    );
+  });
+
+  month.addEventListener("click", () => add_prayer_time(prayerDate));
+
+  select.addEventListener("change", ({ target }) => {
+    prayerDate.filter((i) => {
+      i.date.readable == target.value ? add_prayer_time([i]) : "";
+    });
+  });
+}
+
+function add_time_on_select(prayerDate) {
+  for (let i of prayerDate) {
+    select.innerHTML += `
+            <option value='${i.date.readable}'>${i.date.readable}</option>
+        `;
+  }
+}
+
+function get_loader() {
+  const body = document.querySelector("body");
+  const loader = document.querySelector(".loader");
+  const mainContainer = document.querySelector(".main-container");
+
+  body.classList.remove("hidden");
+  loader.classList.remove("active");
+  mainContainer.classList.remove("blur");
+}
+
+// const table = document.querySelector(".table");
+// const tbody = document.querySelector(".tbody");
+// let data = JSON.parse(localStorage.getItem("student"));
+
+// function addDateHtml() {
+//   if (data) {
+//     tbody.innerHTML = "";
+//     for (let i of data) {
+//       const tr = document.createElement("tr");
+//       tr.innerHTML = `
+//         <th>${i?.id}</th>
+//         <td>${i?.firstname}</td>
+//         <td>${i?.lastname}</td>
+//         <td>${i?.subject}</td>
+//         <td>${i?.age}</td>
+//         <td><button class="btn btn-warning" onclick="deleteStudent(${i?.id})"><i class="fa-solid fa-pen-to-square me-2"></i>Edit</button></td>
+//         <td><button class="btn btn-danger" onclick="deleteStudent(${i?.id})"><i class="fa-solid fa-trash-can me-2"></i>Delete</button></td>
+//       `;
+
+//       tbody.append(tr);
+//     }
+//     table.append(tbody);
+//   } else localStorage.setItem("student", JSON.stringify([]));
+// }
+
+// function createNewStudent() {
+// 	console.log("hi");
+//   const formInput = document.querySelectorAll(".form-control");
+//   let dataLen = data[data.length - 1];
+//   let studentObj = { id: dataLen && dataLen.id > 0 ? dataLen.id + 1 : 1 };
+//   for (let i of formInput) {
+//     studentObj[i.name] = i.value;
+//     i.value = "";
+//   }
+
+//   data.push(studentObj);
+//   localStorage.setItem("student", JSON.stringify(data));
+
+//   addDateHtml();
+// }
+
+// function deleteStudent(id) {
+// //   let deleteId = [];
+//   let deleteId =  data?.map((j) => (j.id != id ? j : ""));
+
+//   localStorage.setItem("student", JSON.stringify(deleteId))
+
+//   deleteId = []
+
+//   addDateHtml()
+// }
+
+
+// addDateHtml();
+
+
